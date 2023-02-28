9b104d696846b55b0526bcbe727704e2e479c362
Danila <nonier1108@gmail.com>
2023-02-28 14:08:25 +0300

    init commit

---
+++ titanic-ai/src/main/java/ru/tvgu/TitanicAiApplication.java
@@ -0,0 +1,13 @@
+package ru.tvgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanicAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanicAiApplication.class, args);
+	}
+
+}
---
+++ titanic-ai/src/test/java/ru/tvgu/TitanicAiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tvgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
